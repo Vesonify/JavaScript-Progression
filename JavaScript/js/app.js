0151c2bfb27d4186bc8c866bf80e6fdbf2040d92
Vesonify <68790421+Vesonify@users.noreply.github.com>
2021-10-10 07:55:03 +0500

    Add files via upload

--- JavaScript/js/app.js
+++ JavaScript/js/app.js
@@ -1,4 +1,17 @@
+let countEl = document.getElementById("count-el")
+let count = 0
+
+let saveEl = document.getElementById("save-el")
+
+
 function increment(){
-    let myCount = 0
-    myCount += 1
+   count += 1
+   countEl.textContent = count
+}
+
+function save(){
+   let countStr = count + " - "
+   saveEl.textContent += countStr
+   countEl.textContent = 0
+
 }
